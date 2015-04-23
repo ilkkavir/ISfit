0a57cef5d61e66019e236ddf7ec8b33a48da8c74
Ilkka Virtanen <ilkka.i.virtanen@oulu.fi>
2015-04-23 14:44:42 +0300

    an update to ISparamScales

--- R/ISparamScales.R
+++ R/ISparamScales.R
@@ -19,7 +19,8 @@ ISparamScales <- function(param,nIon){
 
   # do not allow temperature steps smaller than 300 K, this
   # is mainly to keep reasonable width for the prior distributions
-  parScales[2:5] <- pmax(parScales[2:5],300)
+#  parScales[2:5] <- pmax(parScales[2:5],300)
+  parScales[2:5] <- max(parScales[2:5],300)
 
   # make sure that the collision frequency steps are non-zero
   parScales[6] <- max(parScales[6],10)
