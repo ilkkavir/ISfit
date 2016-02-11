4770af3515cc04eb8eca79c91fce11cf33534c01
Ilkka Virtanen <ilkka.i.virtanen@oulu.fi>
2016-02-11 16:12:55 +0200

    switched to the collision frequency model of Brekke & Hall (which is from Schunk & Nagy??)

--- R/Nwind.R
+++ R/Nwind.R
@@ -22,7 +22,8 @@ Nwind <- function(E,Ecov,vi,vicov,h,B,time,lat,lon){
 
     date <- c(time[["year"]]+1900,time[["mon"]]+1,time[["mday"]],time[["hour"]],time[["min"]],time[["sec"]])
     echarge <- 1.60217662e-19
-    mion <- 30.5
+    #mion <- 30.5
+    mion <- c( 'i1'=30.5 , 'i2'=16 )
     u <- 1.66053904e-27
 
     # pad E and Ecov with zeros
@@ -65,17 +66,49 @@ Nwind <- function(E,Ecov,vi,vicov,h,B,time,lat,lon){
         dimnames(ViBxyzCov) <- NULL
 
         ### end of note...
+
+        # compositions etc. 
         ptmp <- iriParams( time=date ,latitude=lat[hh],longitude=lon[hh],heights=h[hh])
-        # ion-neutral collision frequency
-        ioncoll <- sum( ionNeutralCollisionFrequency(ptmp[,1])['NO+',] )
-        # ion gyro-frequency
-        iongyro <- echarge*Bstrength/( mion*u)
-        # pedersen conductivity
-        kp <- (iongyro*ioncoll)/(echarge*Bstrength*(iongyro**2+ioncoll**2))
-        # hall conductivity
-        kh <- (iongyro**2)/(echarge*Bstrength*(iongyro**2+ioncoll**2))
-        # parallel conductivity
-        kb <- 1/(mion*u*ioncoll)
+
+#        # this was with collision frequencies from Banks et al.  and only heavy ions..
+#        if(FALSE){
+#        # ion-neutral collision frequency
+#        ioncoll <- sum( ionNeutralCollisionFrequency(ptmp[,1])['NO+',] )
+#        # ion gyro-frequency
+#        iongyro <- echarge*Bstrength/( mion*u)
+#        # pedersen conductivity
+#        kp <- (iongyro*ioncoll)/(echarge*Bstrength*(iongyro**2+ioncoll**2))
+#        # hall conductivity
+#        kh <- (iongyro**2)/(echarge*Bstrength*(iongyro**2+ioncoll**2))
+#        # parallel conductivity
+#        kb <- 1/(mion*u*ioncoll)
+#        }
+
+        # better collision frequencies and two ions
+        ioncoll <- ionNeutralCollisionFrequency2(ptmp[,1])
+        # gyrofrequencies for both ion species
+        iongyro <- echarge * Bstrength / ( mion * u )
+        # pedersen conductivities, use the element names to make sure that everything is in correct order
+        kp1 <- ( iongyro[c('i1','i2')] * ioncoll[c('i1','i2')] ) /
+            ( echarge * Bstrength * ( iongyro[c('i2','i2')]**2 + ioncoll[c('i1','i2')]**2 ) ) 
+        # hall conductivities
+        kh1 <- ( iongyro[c('i1','i2')]**2 ) /
+            ( echarge * Bstrength * ( iongyro[c('i1','i2')]**2 + ioncoll[c('i1','i2')]**2 ) )
+        # parallel conductivities
+        kb1 <- 1 / ( mion[c('i1','i2')] * u * ioncoll[c('i1','i2')] )
+
+        # fractions of i1 and i2 from IRI model
+        comp <- ptmp['O+',1] / sum(ptmp[c('NO+','O2+','O+'),1])
+        comp <- c( (1-comp) , comp )
+        names(comp) <- c('i1','i2')
+        comp <- comp / sum(comp)
+
+        # total conductivities
+        kp <- sum( comp * kp1 )
+        kh <- sum( comp * kh1 )
+        kb <- sum( comp * kb1)
+
+        
         # inverse of ion mobility tensor
         mobi <- matrix(0,ncol=3,nrow=3)
         mobi[1,1] <- kp/(kh**2+kp**2)
@@ -90,9 +123,15 @@ Nwind <- function(E,Ecov,vi,vicov,h,B,time,lat,lon){
         VimobCov <- mobi%*%ViBxyzCov%*%t(mobi)
 
         # subtract the electric field contribution and divide by mion*u*ioncoll
-        nWind[hh,] <- ( Vimob - echarge*E2 ) / (mion*u*ioncoll)
-        nWindCov[hh,,] <- ( VimobCov + echarge**2*Ecov2 ) / (mion*u*ioncoll)**2
+#        if(FALSE){
+#        nWind[hh,] <- ( Vimob - echarge*E2 ) / (mion*u*ioncoll)
+#        nWindCov[hh,,] <- ( VimobCov + echarge**2*Ecov2 ) / (mion*u*ioncoll)**2
+#        }
+        nWind[hh,] <- ( Vimob - echarge*E2 ) / sum( comp * mion[c('i1','i2')] * u * ioncoll[c('i1','i2')])
+        nWindCov[hh,,] <- ( VimobCov + echarge**2*Ecov2 ) /
+            sum( comp * mion[c('i1','i2')] * u * ioncoll[c('i1','i2')] )**2
 
+        
         # rotate to ENU coordinates
         rotMati <- solve(rotMat)
         nWind[hh,] <- rotMati%*%nWind[hh,]
